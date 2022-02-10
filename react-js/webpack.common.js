01d263409c7db815ea24dd68aa7120cc52388369
ishubham21 <sg2199203@gmail.com>
2022-02-10 14:28:20 +0530

    [react-js]: add webpack configs

--- react-js/webpack.common.js
+++ react-js/webpack.common.js
@@ -2,45 +2,68 @@ const path = require('path')
 const HTMLWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
+//directories
+const BUILD_DIR = path.join(__dirname, 'build');
+const APP_DIR = path.join(__dirname, 'src');
+
+const VENDOR_LIBS = ['react', 'react-dom',];
+
 module.exports = {
-    mode: 'development',
-    entry: './src/index.js',
+    entry: {
+        app: APP_DIR + "/index.js",
+        vendor: VENDOR_LIBS
+    },
     module: {
         rules: [
             {
+                //for JavaScript
                 test: /\.js$/,
                 exclude: /node_modules/,
                 use: 'babel-loader'
             },
             {
+                //for CSS
                 test: /\.css$/i,
-                use: [MiniCssExtractPlugin.loader, "css-loader"],
+                use: [MiniCssExtractPlugin.loader, 'css-loader'],
             },
             {
+                //for images
                 test: /\.(jpg|svg|png|jpe?g|gif)$/i,
                 use: [
                     {
                         loader: 'file-loader',
+                        options: {
+                            outputPath: 'images',
+                        }
                     },
                 ],
             },
+            {
+                //For fonts
+                test: /\.(woff|woff2|ttf|otf|eot)$/,
+                use: [
+                  {
+                    //using file-loader too
+                    loader: 'file-loader',
+                    options: {
+                      outputPath: 'fonts'
+                    }
+                  }
+                ]
+              },
         ]
     },
     plugins: [
+        //for HTML
         new HTMLWebpackPlugin({
-            template: path.resolve(__dirname, "public", "index.html"),
+            template: path.resolve(__dirname, 'public', 'index.html'),
             inject: 'body'
         }),
         new MiniCssExtractPlugin(),
     ],
     output: {
         path: path.resolve(__dirname, 'build'),
-        filename: "[name].js",
+        filename: '[name].[hash].js',
+        publicPath: '/'
     },
-    devtool: 'inline-source-map',
-    devServer: {
-        port: 3002,
-        open: true,
-        compress: true,
-    }
 }
---
+++ react-js/webpack.prod.js
@@ -0,0 +1,19 @@
+const { merge } = require('webpack-merge')
+const common = require('./webpack.common')
+const TerserPlugin = require('terser-webpack-plugin')
+
+module.exports = merge(common, {
+    mode: 'production',
+    devtool: 'source-map',
+    optimization: {
+        splitChunks: {
+            chunks: 'all'
+        },
+        minimize: true,
+        minimizer: [
+            new TerserPlugin({
+                cache: true
+            })
+        ],
+    },
+})
